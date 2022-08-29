846d2068710d07447821e2a1aaf7253b1406ef56
Valeentin87 <Valeentin87@list.ru>
2022-08-29 22:38:57 +0300

    переработал с учётом замечангий Виталия

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,21 +1,28 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 Console.WriteLine("Пожалуйства введите количество цифр, которое будете вводить");
 int N = Convert.ToInt32(Console.ReadLine());
-int NPositive = InputPositiveNumber(N);
-Console.WriteLine($"количество положительных чисел из введенных равно {NPositive}");
-int InputPositiveNumber(int Nnumber)
+int[] UserNumber = InputNumber(N);
+int result = PositiveNumber(UserNumber);
+Console.WriteLine($"количество положительных чисел из введенных равно {result}");
+int[] InputNumber(int Nnumber)
 {
-    int positive = 0;
     int[] array = new int[Nnumber];
-    for(int i = 0; i < Nnumber; i++)
+    for (int i = 0; i < Nnumber; i++)
     {
         Console.WriteLine("введите очередное число");
         array[i] = Convert.ToInt32(Console.ReadLine());
-        if(array[i] > 0)
+    }
+    return array;
+}
+int PositiveNumber(int[] arrayUser)
+{
+    int positive = 0;
+    for (int i = 0; i < arrayUser.Length; i++)
+    {
+        if (arrayUser[i] > 0)
         {
             positive++;
         }
-
     }
     return positive;
 }
